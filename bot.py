a6f99298cfcf3a4632c97b870cae2e5e48880c9d
nolan7512 <75007954+nolan7512@users.noreply.github.com>
2023-11-08 09:19:53 +0700

    Update bot.py
    bot final

--- bot.py
+++ bot.py
@@ -5,92 +5,123 @@ Created on Tue Nov  7 13:43:46 2023
 @author: TuanKiet-Nguyen
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  4 08:36:26 2023
 
-@author: TuanKiet-Nguyen
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  3 16:22:14 2023
-
-@author: TuanKiet-Nguyen
-"""
 
 import os
 from telethon.sync import events,errors,TelegramClient
 
 
 # from telethon import TelegramClient
-CHANNEL_USERNAMES = '@hamsuperpips,@hamsupergold,@signalforopentradebot'
-api_id = 26403417
-api_hash = '33c9a5d269bc49c2dd7fbceda38b3a4f'
-channel_usernames = CHANNEL_USERNAMES.split(',')
-your_channel_username = '@signalforopentradebot'
-phone_number = 84973399573
-bot_token  ='6386799203:AAF0FmGumSN40PsQfqEJ2rsHKk0dyAjQ4Oo'
-
-
-
+api_id = int(os.environ.get("API_ID", 0))
+api_hash = os.environ.get('API_HASH')
+channel_usernames = os.environ.get('CHANNEL_USERNAMES').split(',')
+your_channel_username = os.environ.get('YOUR_CHANNEL_USERNAME')
+phone_number = int(os.environ.get("PHONE_NUMBER", 0))
+pass_code = int(os.environ.get("PASS_CODE", 0))
+bot_token = os.environ.get('BOT_TOKEN')
+session_path = '/etc/secrets/bot_session_online.session'
+session_paths ='./bot_session_online.session'
+ # Danh sách các từ cần lọc
+filter_words = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD', 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY', 'XAGUSD', 'XAUUSD','GOLD','AUD/CAD', 'AUD/CHF', 'AUD/JPY', 'AUD/NZD', 'AUD/USD', 'CAD/CHF', 'CAD/JPY', 'CHF/JPY', 'EUR/AUD', 'EUR/CAD', 'EUR/CHF', 'EUR/GBP', 'EUR/JPY', 'EUR/NZD', 'EUR/USD', 'GBP/AUD', 'GBP/CAD', 'GBP/CHF', 'GBP/JPY', 'GBP/NZD', 'GBP/USD', 'NZD/CAD', 'NZD/CHF', 'NZD/JPY', 'NZD/USD', 'USD/CAD', 'USD/CHF', 'USD/JPY', 'XAG/USD', 'XAU/USD']
+filter_mode = False  # Trạng thái chế độ lọc
+status_message = "Bot stopped, Filter mode: OFF"
 
 
 try:
     print('Starting connect')
-    client = TelegramClient('bot_session', api_id, api_hash)
-    client.start(bot_token=bot_token)
+    client = TelegramClient(session_paths, api_id, api_hash)
+    client.start(phone=phone_number, password=pass_code)
+    #client.start(bot_token=bot_token)
     #client.connect()
 except OSError:
     print('Failed to connect')
-
-
-
-if not client.is_user_authorized():
-    print("Kết nối fail.")
-    # client.send_code_request(phone_number)
-    # me = client.sign_in(phone_number, input('Enter code: '))
 bot_active = True  # Biến để kiểm tra trạng thái hoạt động của bot
+
 
 @client.on(events.NewMessage(chats=channel_usernames))
 async def forward_message(event):
     if bot_active:
-        original_message = event.message
-        if original_message.reply_to_msg_id:
-            # Nếu tin nhắn gốc là một reply
-            original_reply_msg_id = original_message.reply_to_msg_id
-            original_reply_message = await client.get_messages(event.chat_id, ids=original_reply_msg_id)
-            new_message = await event.respond(original_message.text, reply_to=original_reply_message)
-        else:
-            new_message = await event.respond(original_message.text)
-        await client.forward_messages(your_channel_username, new_message)
+        message_text = event.message.text
+        if not filter_mode or any(word in message_text for word in filter_words):
+            await client.send_message(your_channel_username, message_text)
 
-@client.on(events.NewMessage(pattern=r'^/start$'))
+@client.on(events.NewMessage(pattern=r'^/startfw$'))
 async def handle_start_command(event):
-    global bot_active
+    global bot_active, status_message
     if not bot_active:
         bot_active = True
-        await event.respond('Bot started.')
-    else:
-        await event.respond('Bot is already running.')
+        filter_status = "ON" if filter_mode else "OFF"
+        status_message = f"Bot started, Filter mode: {filter_status}"
+        await event.respond(status_message)
 
-@client.on(events.NewMessage(pattern=r'^/stop$'))
+@client.on(events.NewMessage(pattern=r'^/stopfw$'))
 async def handle_stop_command(event):
-    global bot_active
+    global bot_active, status_message
     if bot_active:
         bot_active = False
-        await event.respond('Bot stopped.')
+        filter_status = "ON" if filter_mode else "OFF"
+        status_message = f"Bot stopped, Filter mode: {filter_status}"
+        await event.respond(status_message)
 
+@client.on(events.NewMessage(pattern=r'^/helpfw$'))
+async def handle_help_command(event):    
+    help_message = "Hướng dẫn sử dụng Bot Forward:\n\n"
+    help_message += "/startfw: Bắt đầu hoạt động của bot.\n"
+    help_message += "/stopfw: Dừng hoạt động của bot.\n"
+    help_message += "/helpfw: Hiển thị hướng dẫn này.\n"
+    help_message += "/filteron: Bật chế độ lọc từ trong tin nhắn.\n"
+    help_message += "/filteroff: Tắt chế độ lọc từ trong tin nhắn.\n"
+    help_message += "/statusfw: Hiển thị trạng thái của bot và chế độ lọc từ.\n"
+    help_message += "/addchannel tenchannel:\n"
+    help_message += "     Để thêm một kênh, sử dụng lệnh sau:\n      /addchannel ten_kenh có @ trước tên\n"
+    help_message += "     Ví dụ: /addchannel @mdex\n"
+    help_message += "/deletechannel tenchannel: \n"
+    help_message += "     Để thêm một kênh, sử dụng lệnh sau:\n      /deletechannel ten_kenh có @ trước tên\n"
+    help_message += "     Ví dụ: /addchannel @mdex\n"
+    help_message += "/listchannels: Liệt kê danh sách các kênh hiện tại.\n"
+    await event.respond(help_message)
 
+@client.on(events.NewMessage(pattern=r'^/filteron$'))
+async def handle_filteron_command(event):
+    global filter_mode, status_message
+    filter_mode = True
+    status_message = "Bot started, Filter mode: ON"
+    await event.respond(status_message)
 
-# try:
-#     client.start(bot_token=bot_token)
-# except OSError:
-#     print('Failed bot_api to connect')
+@client.on(events.NewMessage(pattern=r'^/filteroff$'))
+async def handle_filteroff_command(event):
+    global filter_mode, status_message
+    filter_mode = False
+    status_message = "Bot started, Filter mode: OFF"
+    await event.respond(status_message)
 
-# try:
-#     client.start()
-# except OSError:
-#     print('Failed start to connect')
+@client.on(events.NewMessage(pattern=r'^/statusfw$'))
+async def handle_status_command(event):
+    await event.respond(status_message)
+
+@client.on(events.NewMessage(pattern=r'^/addchannel'))
+async def handle_add_channel_command(event):
+    channel_name = event.raw_text.split('@')[1]
+    if channel_name and channel_name not in channel_usernames:
+        channel_usernames.append(channel_name)
+        os.environ['CHANNEL_USERNAMES'] = ','.join(channel_usernames)
+        await event.respond(f"@{channel_name} đã được thêm vào danh sách kênh.")
+    else:
+        await event.respond(f"@{channel_name} đã có trong danh sách kênh hoặc tên kênh không hợp lệ.")
+
+@client.on(events.NewMessage(pattern=r'^/deletechannel'))
+async def handle_delete_channel_command(event):
+    channel_name = event.raw_text.split('@')[1]
+    if channel_name in channel_usernames:
+        channel_usernames.remove(channel_name)
+        os.environ['CHANNEL_USERNAMES'] = ','.join(channel_usernames)
+        await event.respond(f"@{channel_name} đã bị xóa khỏi danh sách kênh.")
+    else:
+        await event.respond(f"@{channel_name} không có trong danh sách kênh.")
+
+@client.on(events.NewMessage(pattern=r'^/listchannel'))
+async def handle_list_channel_command(event):
+    channel_list = ', '.join(channel_usernames)
+    await event.respond(f"Danh sách các kênh hiện tại: {channel_list}")
 
 client.run_until_disconnected()
